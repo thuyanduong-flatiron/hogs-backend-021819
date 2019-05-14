7fd6bf0a4118f27e7e1c79416e10d51016df1966
thuyanduong-flatiron <thuyanannduong@gmail.com>
2019-05-14 17:28:14 -0400

    ypo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://anns-hogs-frontend.herokuapp.com/'
+    origins 'https://anns-hogs-frontend.herokuapp.com'
 
     resource '*',
       headers: :any,
